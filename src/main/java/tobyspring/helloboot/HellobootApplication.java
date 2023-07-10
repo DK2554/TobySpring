ffda893ab07fc63fb9885b8b7165c61af7d69bdb
DK2554 <ldu0215@gmail.com>
2023-07-10 21:37:28 +0900

    수정 : 토비 스프링 기존 코드 제거 및 개인 깃 허브 푸쉬

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,26 +1,56 @@
 package tobyspring.helloboot;
 
+import org.apache.catalina.servlets.WebdavServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@SpringBootApplication
-public class HellobootApplication {
-    private final JdbcTemplate jdbcTemplate;
 
-    public HellobootApplication(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    @PostConstruct
-    void init() {
-        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-    }
-
+//@SpringBootApplication
+public class  HellobootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
-    }
 
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        //빈을 등록하기 위해서 GenericApplicationContext을 사용해서 HellocController라는 클래스를 빈으로 생성해서 getBean을 통해 사용한다
+
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("frontController", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                   // 인증, 보안, 다국어, 공통기능
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+                        String name =  req.getParameter("name");
+                        HelloController controller=applicationContext.getBean(HelloController.class);
+                        String ret = controller.hello(name);
+
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+                    }else{
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
+
+                }
+            }).addMapping("/*");
+        });
+        webServer.start();
+    }
 }
